85787bb7e15183ace2ec158a18a27068ea3f7cbf
Prince-Saha-Ayon <67217982+Prince-Saha-Ayon@users.noreply.github.com>
2022-08-26 23:59:04 +0600

    resources

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="card">
+    <div class="card-body">
+        <h4>Category Page</h4>
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Image</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($category as $item)
+                <tr>
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->description }}</td>
+                    <td>
+                        <img src="{{ asset('asset/uploads/category/'.$item->image) }}" alt="Image here">
+
+                    </td>
+                    <td>
+                        <button class="btn btn-primary">Edit</button>
+                        <button class="btn btn-danger">Delete</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
