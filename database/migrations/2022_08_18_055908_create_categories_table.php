dd762ef5af1c19eab6acd97f7a3e449f1de9dde5
Prince-Saha-Ayon <67217982+Prince-Saha-Ayon@users.noreply.github.com>
2022-08-26 23:54:07 +0600

    database

---
+++ database/migrations/2022_08_18_055908_create_categories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Ramsey\Uuid\v1;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->longText('description');
+            $table->tinyInteger('status')->default('0');
+            $table->tinyInteger('popular')->default('0');
+            $table->string('image');
+            $table->string('meta_title');
+            $table->string('meta_description');
+            $table->string('meta_keywords');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
